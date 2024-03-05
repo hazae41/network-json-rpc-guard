7fa023047d8c7520ec9f2566fa714e5b526e3917
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-05 22:42:49 +0100

    signal

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 // deno-lint-ignore-file no-empty require-await
 import * as Dotenv from "https://deno.land/std@0.217.0/dotenv/mod.ts";
 import { Future } from "npm:@hazae41/future@1.0.3";
-import { RpcErr, RpcError, RpcInvalidParamsError, RpcOk, RpcRequest, RpcRequestInit } from "npm:@hazae41/jsonrpc@1.0.5";
+import { RpcCounter, RpcErr, RpcError, RpcInvalidParamsError, RpcOk, RpcRequest, RpcRequestInit, RpcRequestPreinit, RpcResponse, RpcResponseInit } from "npm:@hazae41/jsonrpc@1.0.5";
 import { Mutex } from "npm:@hazae41/mutex@1.2.12";
 import { Memory, NetworkMixin, base16_decode_mixed, base16_encode_lower, initBundledOnce } from "npm:@hazae41/network-bundle@1.2.1";
 import { None, Some } from "npm:@hazae41/option@1.0.27";
@@ -12,29 +12,175 @@ export async function main() {
   const envPath = new URL(import.meta.resolve("./.env.local")).pathname
 
   const {
-    RPC_URL_WS = Deno.env.get("RPC_URL_WS"),
-    RPC_URL_HTTP = Deno.env.get("RPC_URL_HTTP"),
+    SELF_URL_WS = Deno.env.get("SELF_URL_WS"),
+    SELF_URL_HTTP = Deno.env.get("SELF_URL_HTTP"),
+    SERVER_URL_WS = Deno.env.get("SERVER_URL_WS"),
+    SERVER_URL_HTTP = Deno.env.get("SERVER_URL_HTTP"),
+    SERVER_RPC_METHODS = Deno.env.get("SERVER_RPC_METHODS"),
     PRIVATE_KEY_ZERO_HEX = Deno.env.get("PRIVATE_KEY_ZERO_HEX"),
   } = await Dotenv.load({ envPath, examplePath: null })
 
+  if (SELF_URL_WS != null && SERVER_URL_WS == null)
+    throw new Error("SERVER_URL_WS is not set")
+  if (SELF_URL_HTTP != null && SERVER_URL_HTTP == null)
+    throw new Error("SERVER_URL_HTTP is not set")
+  if (SERVER_RPC_METHODS == null)
+    throw new Error("SERVER_RPC_METHODS is not set")
   if (PRIVATE_KEY_ZERO_HEX == null)
     throw new Error("PRIVATE_KEY_ZERO_HEX is not set")
 
-  const rpcUrlWs = RPC_URL_WS
-  const rpcUrlHttp = RPC_URL_HTTP
+  const selfUrlWs = SELF_URL_WS
+  const selfUrlHttp = SELF_URL_HTTP
+
+  const serverUrlWs = SERVER_URL_WS
+  const serverUrlHttp = SERVER_URL_HTTP
+  const serverRpcMethods = SERVER_RPC_METHODS.split(",")
+
   const privateKeyZeroHex = PRIVATE_KEY_ZERO_HEX
 
-  return await serve({ privateKeyZeroHex, rpcUrlHttp, rpcUrlWs })
+  return await serve({ selfUrlWs, selfUrlHttp, serverUrlWs, serverUrlHttp, serverRpcMethods, privateKeyZeroHex })
+}
+
+export async function signal(url: string, params: { selfUrlWs?: string, selfUrlHttp?: string, serverRpcMethods: string[] }) {
+  const { selfUrlWs, selfUrlHttp, serverRpcMethods } = params
+
+  while (true) {
+    try {
+      const socket = new WebSocket(`${url}/?session=${crypto.randomUUID()}`)
+
+      await new Promise((ok, err) => {
+        socket.addEventListener("open", ok)
+        socket.addEventListener("error", err)
+      })
+
+      const counter = new RpcCounter()
+      const events = new EventTarget()
+
+      let balanceBigInt = 0n
+
+      const onRequest = (request: RpcRequest<unknown>) => {
+        events.dispatchEvent(new CustomEvent("request", { detail: request }))
+      }
+
+      const onResponse = (response: RpcResponse<unknown>) => {
+        events.dispatchEvent(new CustomEvent("response", { detail: response }))
+      }
+
+      const onMessage = (message: string) => {
+        const requestOrResponse = JSON.parse(message) as RpcRequest<unknown> | RpcResponse
+
+        if ("method" in requestOrResponse)
+          return onRequest(requestOrResponse)
+
+        return onResponse(requestOrResponse)
+      }
+
+      socket.addEventListener("message", (event) => {
+        if (typeof event.data !== "string")
+          return
+        return onMessage(event.data)
+      })
+
+      const requestOrThrow = async <T>(preinit: RpcRequestPreinit<unknown>, price: bigint) => {
+        const request = counter.prepare(preinit)
+        const message = JSON.stringify(request)
+
+        balanceBigInt -= price
+
+        while (balanceBigInt < 0n)
+          await net_tip()
+
+        socket.send(message)
+
+        return await new Promise<RpcResponse<T>>(ok => {
+          const onResponse = async (event: Event) => {
+            const response = (event as CustomEvent<RpcResponseInit<T>>).detail
+
+            if (response.id !== request.id)
+              return
+            ok(RpcResponse.from(response))
+          }
+
+          events.addEventListener("response", onResponse)
+        })
+      }
+
+      const net_tip = async () => {
+        const {
+          chainIdString,
+          contractZeroHex,
+          receiverZeroHex,
+          nonceZeroHex,
+          minimumZeroHex
+        } = await requestOrThrow<{
+          chainIdString: string,
+          contractZeroHex: string,
+          receiverZeroHex: string,
+          nonceZeroHex: string,
+          minimumZeroHex: string
+        }>({
+          method: "net_get"
+        }, 0n).then(r => r.unwrap())
+
+        const minimumBigInt = BigInt(minimumZeroHex)
+
+        if (minimumBigInt > (2n ** 24n))
+          throw new Error("Minimum too high")
+
+        const chainIdBase16 = Number(chainIdString).toString(16).padStart(64, "0")
+        const chainIdMemory = base16_decode_mixed(chainIdBase16)
+
+        const contractBase16 = contractZeroHex.slice(2).padStart(64, "0")
+        const contractMemory = base16_decode_mixed(contractBase16)
+
+        const receiverBase16 = receiverZeroHex.slice(2).padStart(64, "0")
+        const receiverMemory = base16_decode_mixed(receiverBase16)
+
+        const nonceBase16 = nonceZeroHex.slice(2).padStart(64, "0")
+        const nonceMemory = base16_decode_mixed(nonceBase16)
+
+        const mixinStruct = new NetworkMixin(chainIdMemory, contractMemory, receiverMemory, nonceMemory)
+
+        const minimumBase16 = minimumZeroHex.slice(2).padStart(64, "0")
+        const minimumMemory = base16_decode_mixed(minimumBase16)
+
+        const generatedStruct = mixinStruct.generate(minimumMemory)
+
+        const secretMemory = generatedStruct.to_secret()
+        const secretBase16 = base16_encode_lower(secretMemory)
+        const secretZeroHex = `0x${secretBase16}`
+
+        balanceBigInt += await requestOrThrow<string>({ method: "net_tip", params: [secretZeroHex] }, 0n).then(r => BigInt(r.unwrap()))
+      }
+
+      if (selfUrlWs != null)
+        await requestOrThrow({ method: "net_signal", params: [crypto.randomUUID(), { url: selfUrlWs, protocol: "json-rpc-over-wss", methods: serverRpcMethods }] }, (20n ** 20n))
+      if (selfUrlHttp != null)
+        await requestOrThrow({ method: "net_signal", params: [crypto.randomUUID(), { url: selfUrlHttp, protocol: "json-rpc-over-https", methods: serverRpcMethods }] }, (20n ** 20n))
+
+      await new Promise(ok => socket.addEventListener("close", ok))
+      continue
+    } catch (e: unknown) {
+      console.warn(`Could not signal to ${url}`, e)
+      await new Promise(ok => setTimeout(ok, 5000))
+      continue
+    }
+  }
 }
 
 export async function serve(params: {
+  selfUrlWs?: string,
+  selfUrlHttp?: string,
+  serverUrlHttp?: string,
+  serverUrlWs?: string,
+  serverRpcMethods: string[],
   privateKeyZeroHex: string,
-  rpcUrlHttp?: string,
-  rpcUrlWs?: string,
 }) {
-  const { privateKeyZeroHex, rpcUrlWs, rpcUrlHttp } = params
+  const { selfUrlWs, selfUrlHttp, serverUrlWs, serverUrlHttp, serverRpcMethods, privateKeyZeroHex } = params
 
   await initBundledOnce()
+
+  signal("wss://signal.node0.hazae41.me", { selfUrlWs, selfUrlHttp, serverRpcMethods }).catch(console.error)
 
   const chainIdString = "100"
   const contractZeroHex = "0x0a4d5EFEa910Ea5E39be428A3d57B80BFAbA52f4"
@@ -228,7 +374,7 @@ export async function serve(params: {
     }
 
     if (request.headers.get("upgrade") !== "websocket") {
-      const target = rpcUrlHttp
+      const target = serverUrlHttp
 
       if (target == null)
         return new Response("Bad Gateway", { status: 502 })
@@ -277,7 +423,7 @@ export async function serve(params: {
       }
     }
 
-    const target = rpcUrlWs
+    const target = serverUrlWs
 
     if (target == null)
       return new Response("Bad Gateway", { status: 502 })
